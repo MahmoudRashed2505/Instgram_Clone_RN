c44e985b3b83a3a148cd9a5e113d629328cb16e1
Ahmedsafwat101 <42112466+Ahmedsafwat101@users.noreply.github.com>
2020-09-28 06:20:00 +0200

    Search is done

--- App.js
+++ App.js
@@ -7,108 +7,48 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { View, Text, StyleSheet, FlatList, TextInput } from 'react-native';
+import SearchItem from './SearchItem'
+import query from './query.json'
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const DATA= query
+ 
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+function App(){
+ 
+  return(
+    <View style={styles.container}>
+      <TextInput style={styles.serachbar} placeholder="Search" placeholderTextColor="#FFF"></TextInput>
+      <FlatList
+       data={DATA}
+                renderItem={({ item}) => {
+                    return (<SearchItem name={item["humanLabel"]} pic={item["picture"]}  />)
+                }}
+                ></FlatList>
+      <SearchItem/>
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    backgroundColor:"#1f1f1f",
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+  serachbar:{
+    paddingLeft:20,
+    fontSize:16,
+    backgroundColor: "#1f1f1f",
+    color:"white",
+    borderWidth:1,
+    borderColor:"#3d3d3d",
+    margin:10,
+    borderRadius:25
+
+  }
+
+  
+})
+
+
 
 export default App;
---
+++ SearchItem.js
@@ -0,0 +1,44 @@
+import React  from 'react';
+import { View, Text, StyleSheet, FlatList, TextInput, Image, TouchableOpacity } from 'react-native';
+
+export default function SearchItem(dataArray){
+    return(
+        <View style={styles.container}>
+        <TouchableOpacity style={styles.item}>
+        <Image source={{uri:dataArray.pic}} style={styles.img} resizeMode="contain" />
+    <Text style={styles.text}>{dataArray.name}</Text>
+        </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles= StyleSheet.create({
+    container:{
+       backgroundColor:"#1f1f1f",
+       alignItems:"center",
+       alignContent:"center",
+       padding:8
+    },
+    item:{
+        backgroundColor:"black",
+        width:"100%",
+        height:100,
+        flexDirection:"row",
+        alignContent:"center",
+        alignItems:"center",
+        borderWidth:2,
+        borderColor:"#3d3d3d",
+        borderRadius:20,
+        padding:15
+    },
+    img:{
+       width:70,
+       height:70,
+      borderRadius:200
+    },
+    text:{
+       color:"white",
+       fontSize:20,
+       marginLeft:20
+    },
+})
