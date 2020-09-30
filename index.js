00ff8f95b26b146e798ef46c076ce76af8c55e47
Ahmedsafwat101 <42112466+Ahmedsafwat101@users.noreply.github.com>
2020-09-30 19:28:19 +0200

    Some Updates

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import routes from './src/Routes/routes';
-import Stories from './src/screens/Stories/StoryComp'
 
 AppRegistry.registerComponent(appName, () => routes);
