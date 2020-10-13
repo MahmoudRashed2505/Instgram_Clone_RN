a97cf9e2eb59fd1ed129f28eb8e7f20bcb58e0d1
nadaaeltayeb <nada.eltayeb.1999@gmail.com>
2020-10-13 20:08:53 +0200

    data fetching from API

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,28 +1,52 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, ActivityIndicator} from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
 import query from '../assets/JSON/Search Query.json';
 import FeedCard from './FeedCard';
 
-const Data = query;
-var imgs = Data.map((item) => {
-  return {uri: item['picture']};
-});
-
 export default function Feed() {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+
+  fetch('https://json.extendsclass.com/bin/6926f9143c99')
+    .then((response) => {
+      //console.log('success', response);
+      return response.json();
+    })
+    .then((responseJson) => {
+      console.log('success', responseJson);
+      setData(responseJson);
+      setLoading(false);
+    })
+    .catch((error) => {
+      console.log('Error:', error);
+    });
+
   return (
-    <View style={{width: '100%', marginLeft: 1, alignSelf:"center"}}>
-      <FlatList
-        data={Data}
-        renderItem={({item}) => {
-          return (
-            <FeedCard
-              username={item['humanLabel']}
-              profilePic={item['picture']}
-            />
-          );
-        }}
-      />
+    <View style={{width: '100%', alignSelf: 'center'}}>
+      {loading ? (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: 80,
+          }}>
+          <ActivityIndicator size="large" color="#bc2b78" />
+        </View>
+      ) : (
+        <FlatList
+          data={data}
+          renderItem={({item}) => {
+            return (
+              <FeedCard
+                username={item['humanLabel']}
+                profilePic={item['picture']}
+              />
+            );
+          }}
+        />
+      )}
     </View>
   );
 }
--- src/screens/Activity/Activity.js
+++ src/screens/Activity/Activity.js
@@ -3,14 +3,13 @@ import {View, Text, StyleSheet, FlatList} from 'react-native';
 import FollowCard from '../../components/FollowCard.js';
 import PostCard from '../../components/PostCard.js';
 import query from '../../assets/JSON/Activity Query.json';
-
+const DATA = query;
 function Activity() {
-  const DATA = query;
   return (
     <View style={styles.container}>
-        <View style={styles.bar}>
-            <Text style={styles.txtBar}>Activity</Text>
-        </View>
+      <View style={styles.bar}>
+        <Text style={styles.txtBar}>Activity</Text>
+      </View>
       <View style={styles.cardContainer}>
         <FollowCard />
       </View>
@@ -31,8 +30,7 @@ function Activity() {
               />
             );
           }}
-                  keyExtractor={(item, index) => index.toString()}
-          ></FlatList>
+          keyExtractor={(item, index) => index.toString()}></FlatList>
       </View>
     </View>
   );
@@ -48,7 +46,6 @@ const styles = StyleSheet.create({
   cardContainer: {
     alignItems: 'center',
     marginTop: 20,
-    
   },
   txt: {
     paddingLeft: 10,
@@ -56,20 +53,20 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: 'black',
   },
-  bar:{
-      width: "100%",
-      height: 55,
-      backgroundColor: "white",
-      //borderBottomWidth:0.5,
-      //borderBottomColor:"black",
-      elevation:5
+  bar: {
+    width: '100%',
+    height: 55,
+    backgroundColor: 'white',
+    //borderBottomWidth:0.5,
+    //borderBottomColor:"black",
+    elevation: 5,
   },
-  txtBar:{
-        padding:20,
-        fontSize:20,
-        fontWeight:"500",
-        color:"black"
-  }
+  txtBar: {
+    padding: 20,
+    fontSize: 20,
+    fontWeight: '500',
+    color: 'black',
+  },
 });
 
 export default Activity;
